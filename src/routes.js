ed2c5c9400a47981b0a53e4c3f53eabc6c8c7a30
Caio Carneiro <caiozinx@gmail.com>
2019-09-01 04:00:12 -0300

    Valicacoes de disponibilidade e ajuste de dados para o front-end

--- src/routes.js
+++ src/routes.js
@@ -9,6 +9,7 @@ import authMiddleware from './app/middleware/auth';
 import AppointmentController from './app/controllers/AppointmentController';
 import ScheduleController from './app/controllers/ScheduleController';
 import NotificationController from './app/controllers/NotificationController';
+import AvailableController from './app/controllers/AvailableController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -21,6 +22,7 @@ routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
+routes.get('/providers/:providerId/available', AvailableController.index);
 
 routes.post('/appointments', AppointmentController.store);
 routes.get('/appointments', AppointmentController.index);
